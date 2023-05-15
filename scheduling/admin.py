eb79d16f67c4a2f89e55f527646ae5052758b824
bjorne90 <bjce90@gmail.com>
2023-05-15 13:48:12 +0000

    Changes to admin.py file inside scheduling

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -5,4 +5,14 @@ class WorkShiftAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_time', 'end_time', 'is_booked', 'role')
     list_display_links = ('name',)  # Add the field you want to link to the change view
 
+    def remove_user(self, obj):
+        if obj.is_booked:
+            obj.user = None
+            obj.is_booked = False
+            obj.save()
+
+    remove_user.short_description = "Remove User from Shift"
+
+    actions = ['remove_user']
+
 admin.site.register(WorkShift, WorkShiftAdmin)
