a860a0fc31ec183fb77386b2734514a658353b36
bjorne90 <bjce90@gmail.com>
2023-05-17 00:53:57 +0000

    Changes to news models.py

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class News(models.Model):
@@ -8,7 +9,7 @@ class News(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
     comments = models.ManyToManyField(User, through='Comment', related_name='commented_posts')
-    image = models.ImageField(upload_to='news/', blank=True, null=True) 
+    image = CloudinaryField('image', blank=True, null=True) 
 
     def __str__(self):
         return self.title
