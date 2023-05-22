9bffc40b101f2229764d81d3967674fecab7bb7d
bjorne90 <bjce90@gmail.com>
2023-05-22 01:12:02 +0000

    Major changes to admin pages, email is now sending, booking more users to shifts, updated time format to 24h time

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,17 +1,32 @@
+from django.db import models
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import WorkShift, Booking
 from scheduling.views import send_email_notification
+from django.contrib.admin.widgets import AdminSplitDateTime
+
+class CustomSplitDateTimeWidget(AdminSplitDateTime):
+    def format_output(self, rendered_widgets):
+        return ''.join(rendered_widgets)
 
 class BookingInline(admin.TabularInline):
     model = Booking
     extra = 1
 
 class WorkShiftAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_time', 'end_time', 'is_booked', 'role')
+    list_display = ('name','formatted_start_time', 'formatted_end_time', 'is_booked', 'role')
     list_display_links = ('name',)
     inlines = [BookingInline]
+
+    def formatted_start_time(self, obj):
+        return obj.start_time.strftime('%Y-%m-%d %H:%M')
+
+    def formatted_end_time(self, obj):
+        return obj.end_time.strftime('%Y-%m-%d %H:%M')
+
+    formatted_start_time.short_description = 'Start Time'
+    formatted_end_time.short_description = 'End Time'
 
     def assign_user(self, request, queryset):
         for workshift in queryset:
@@ -29,5 +44,9 @@ class WorkShiftAdmin(admin.ModelAdmin):
                 workshift.save()
 
     assign_user.short_description = "Assign users to selected workshifts"
+
+    formfield_overrides = {
+    models.DateTimeField: {'widget': CustomSplitDateTimeWidget},
+}
 
 admin.site.register(WorkShift, WorkShiftAdmin)
